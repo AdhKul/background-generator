f208061c8b54c756b270a8d766f087dfd5e48eaa
Kuliscene <adhvaith9@gmail.com>
2021-07-06 23:18:44 +0530

    json

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var _ = require('lodash');
+let  arr=[1,2,3,4,5];
+console.log(_.without(arr,3))
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
